de30897d59f49121788aa640652ea14cca58e157
unknown <john@same.com>
2025-11-06 10:28:02 +0200

    ticket-price complete

---
+++ ticket-price.js
@@ -0,0 +1,14 @@
+visitorAge = 12;
+isStudent = false;
+
+if (visitorAge > 6) {
+    if (isStudent) {
+        console.log("Cena 5 Eur");
+    }
+    else {
+        console.log("Cena 10 Eur");
+    }
+}
+else {
+    console.log("Biļete bezmaksas");
+}
